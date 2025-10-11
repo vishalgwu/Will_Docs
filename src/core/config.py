0b2c117ba646fdef5567193427d35e3c1bd7fee6
Aswin Balaji T R <vishal.fulsundar@gwmail.gwu.edu>
2025-10-11 00:35:17 -0400

    update ser
    updated config.py add serpi and hf token

--- src/core/config.py
+++ src/core/config.py
@@ -6,11 +6,13 @@ load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 SERPER_API_KEY = os.getenv("SERPER_API_KEY")
+HF_TOKEN = os.getenv("HF_TOKEN")
 
 def print_keys_status():
     print(" Environment Loaded:")
     print("OPENAI_API_KEY:", "Found" if OPENAI_API_KEY else "Missing")
     print("SERPER_API_KEY:", "Found" if SERPER_API_KEY else "Missing")
+    print("HF_TOKEN:", "Found" if HF_TOKEN else "Missing")
 
 if __name__ == "__main__":
     print_keys_status()
